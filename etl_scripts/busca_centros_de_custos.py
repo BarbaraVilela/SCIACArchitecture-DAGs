0da55ddcb9e73ae80f9e603a0dfdb2cdc8b5e89b
Bárbara Vilela <barbara.bbmachado@gmail.com>
2022-09-17 10:25:19 -0300

    Dados de centros de custos gravados em um arquivo temporário

--- etl_scripts/busca_centros_de_custos.py
+++ etl_scripts/busca_centros_de_custos.py
@@ -1,9 +1,15 @@
 import requests
 import json
-
+# Fazendo a requisição para a API que retorna a Estrutura Organizacional do Município
 print("Requisição para serviço web do Sistema da Estrutura Organizacional do Município")
 request = requests.get("https://sciac-api-fake.azurewebsites.net/")
-todos = json.loads(request.content)
-print(todos)
+json_data = json.loads(request.content)
+
+# Gravando os dados obtidos em um arquivo temporário
+print("Gravando os dados em um arquivo temporário, a ser consumido pela próxima tarefa")
+temp_file = open("centros_de_custos_temp.txt","w") 
+temp_file.writelines (json.dumps(json_data))
+temp_file.close()
+
 
 print("Executada a coleta dos dados")
