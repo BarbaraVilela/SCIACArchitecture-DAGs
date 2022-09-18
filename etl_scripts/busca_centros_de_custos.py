cc582baec7d12411c07ce46692fab0b75d3f95e4
Bárbara Vilela <barbara.bbmachado@gmail.com>
2022-09-18 11:12:23 -0300

    Conclui a tarefa de busca dos centros de custos

--- etl_scripts/busca_centros_de_custos.py
+++ etl_scripts/busca_centros_de_custos.py
@@ -1,15 +1,18 @@
 import requests
 import json
-# Fazendo a requisição para a API que retorna a Estrutura Organizacional do Município
-print("Requisição para serviço web do Sistema da Estrutura Organizacional do Município")
+
+# Acessa a API do Sistema da Estrutura Organizacional do Município
+print("Faz a requisição GET no serviço web do Sistema da Estrutura Organizacional do Município")
 request = requests.get("https://sciac-api-fake.azurewebsites.net/")
 json_data = json.loads(request.content)
 
-# Gravando os dados obtidos em um arquivo temporário
-print("Gravando os dados em um arquivo temporário, a ser consumido pela próxima tarefa")
-temp_file = open("centros_de_custos_temp.txt","w") 
+print("Grava os dados em um arquivo temporário, a ser consumido pela próxima tarefa")
+# Abre/cria o arquivo e limpa seu conteúdo, caso haja
+temp_file = open("centros_de_custos_temp.txt","r+") 
+temp_file.truncate(0)
+
+# Grava os dados obtidos e fecha o arquivo
 temp_file.writelines (json.dumps(json_data))
 temp_file.close()
 
-
-print("Executada a coleta dos dados")
+print("Coleta de Centros de Custos concluída com sucesso!")
