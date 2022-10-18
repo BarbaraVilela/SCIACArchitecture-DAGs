8d53dea0a098cf0ab9fd78a6f5a5e729db1e6804
Bárbara Vilela <barbara.bbmachado@gmail.com>
2022-10-18 11:52:41 -0300

    Adiciona conexão com o neo4j

--- etlScripts/centrosDeCustos/atualiza_centros_custos.py
+++ etlScripts/centrosDeCustos/atualiza_centros_custos.py
@@ -1,24 +1,42 @@
 # Importando as Bibliotecas necessárias
+from cmath import log
 import json
 import logging
+from neo4j import GraphDatabase 
 
 # Obtendo os dados no arquivo temporário
-log.info('Obtendo os dados armazenados no arquivo temporário pela tarefa anterior')
+logging.info('Obtendo os dados armazenados no arquivo temporário pela tarefa anterior')
 file = '/usr/local/airflow/dags/etlScripts/tempFiles/centros_de_custos_temp.txt'
 with open(file,'r') as temp_file: 
 
     # Lendo os dados e armazenando em uma variável json
     json_data = json.load(temp_file)
-
-    # Estabelece a conexão com o banco de dados e obtém os centros de custos ativos nele armazenados
-    log.info('Obtendo os Centros de Custos ativos armazenados na base')
-
-    # Processa os dados
-    log.info('Processando os dados, tratando as atualizações necessárias')
-
-# Grava as atualizações e fecha o arquivo
-    log.info('Gravando as alterações no arquivo temporário')
-
     temp_file.close()
 
-log.info('Centros de Custos atualizados com sucesso!')
+# Estabelece a conexão com o banco de dados e obtém os centros de custos ativos nele armazenados
+logging.info('Obtendo os Centros de Custos ativos armazenados na base')
+
+# Conectando-se ao banco de dados
+# Credenciais de acesso ao banco
+uri = 'neo4j+s://1258ca96.databases.neo4j.io'
+user = 'neo4j'
+password = 'vay_x2AgwSbsGxbulFpGtJDzAjSo1JnwF_6mVhqPbrU'
+
+grafo = GraphDatabase.driver(uri, auth=(user, password))
+grafo
+
+# Obtém os centros de custos ativos na base
+
+
+
+
+grafo.close()
+
+# Processa os dados
+logging.info('Processando os dados, tratando as atualizações necessárias')
+
+# Grava as atualizações e fecha o arquivo
+logging.info('Gravando as alterações no arquivo temporário')
+
+
+logging.info('Centros de Custos atualizados com sucesso!')
--- etlScripts/centrosDeCustos/carrega_centros_custos.py
+++ etlScripts/centrosDeCustos/carrega_centros_custos.py
@@ -3,7 +3,7 @@ import json
 import logging
 
 # Obtendo os dados no arquivo temporário
-log.info('Obtendo os dados armazenados no arquivo temporário pela tarefa anterior')
+logging.info('Obtendo os dados armazenados no arquivo temporário pela tarefa anterior')
 file = '/usr/local/airflow/dags/etlScripts/tempFiles/centros_de_custos_temp.txt'
 with open(file,'r') as temp_file: 
 
@@ -12,6 +12,6 @@ with open(file,'r') as temp_file:
     temp_file.close()
 
 # Estabelece a conexão com o banco de dados e carrega os dados atualizados
-log.info('Gravando as alterações na base')
+logging.info('Gravando as alterações na base')
 
-log.info('Centros de Custos carregados com sucesso!')
+logging.info('Centros de Custos carregados com sucesso!')
